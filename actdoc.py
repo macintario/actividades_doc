04385ee142cc140bd55092a5b640b07b211557c3
macintario <macintario@gmail.com>
2019-01-09 12:51:10 -0600

    primera titeración.

--- actdoc.py
+++ actdoc.py
@@ -2,8 +2,36 @@ import csv
 import os
 #clean all
 os.remove('salida.csv')
+des_hrs_activ = ''
+cve_act = ''
+actividad = ''
+lugar = ''
+d_lunes = ''
+d_martes = ''
+d_miercoles = ''
+d_jueves = ''
+d_viernes = ''
+d_sabado = ''
+d_domingo = ''
+des_hrs_activ = ''
+
+def limpia_escalerita():
+    des_hrs_activ = ''
+    cve_act = ''
+    actividad = ''
+    lugar = ''
+    d_lunes = ''
+    d_martes = ''
+    d_miercoles = ''
+    d_jueves = ''
+    d_viernes = ''
+    d_sabado = ''
+    d_domingo = ''
+    des_hrs_activ = ''
+
 
 with open('1016.csv') as csvfile:
+    limpia_escalerita()
     act = csv.reader(csvfile, delimiter = ',' )
     for row in act:
         pla_id = row[11][0:4]
@@ -21,7 +49,6 @@ with open('1016.csv') as csvfile:
         id_pa = row[27]
         hrs_base = row[42]
         hrs_interinato = row[41]
-        cve_act = row[68]
         pza_num_emp = row[25]
         id_grupo = row[36]
         id_mod = row[37]
@@ -37,7 +64,26 @@ with open('1016.csv') as csvfile:
 ## la m'endiga escalerita
         if row[62] != '':
             des_hrs_activ = row[62]
-
+        if row[68] != '':
+            cve_act = row[68]
+        if row[69] != '':
+            actividad = row[69]
+        if row[70] != '':
+            lugar = row[70]
+        if row[71] != '':
+            d_lunes = row[71]
+        if row[72] != '':
+            d_martes = row[72]
+        if row[73] != '':
+            d_miercoles = row[73]
+        if row[74] != '':
+            d_jueves = row[74]
+        if row[75] != '':
+            d_viernes = row[75]
+        if row[75] != '':
+            d_sabado = row[76]
+        if row[75] != '':
+            d_domingo = row[77]
         des_cic_id = row[80]
         if asi_id != '':
             with open('salida.csv', mode='a') as salida:
@@ -47,7 +93,13 @@ with open('1016.csv') as csvfile:
                                     hrs_base, hrs_interinato, id_grupo, id_mod, id_salon,
                                     c_lunes, c_martes, c_miercoles, c_jueves, c_viernes, c_sabado, c_domingo, hrs_total])
         if des_cic_id != '':
-            print(des_hrs_activ)
+            with open('salida.csv', mode='a') as salida:
+                linewrite = csv.writer(salida, delimiter=',')
+                linewrite.writerow([pla_id, plantel, nombre_docente, id_docente,horas_min,
+                                    horas_max, turno, cve_act, actividad, hrs_asig, tipo_asig, semestre, id_pa, ' ',
+                                    hrs_base, hrs_interinato, id_grupo, 'A', lugar,
+                                    d_lunes, d_martes, d_miercoles, d_jueves, d_viernes, d_sabado, d_domingo, des_hrs_activ])
         if pza_num_emp != '':
             print(pza_num_emp)
+            limpia_escalerita()
 print("ok")
