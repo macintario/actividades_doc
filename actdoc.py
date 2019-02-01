ea32725248a94aa64a50f39818c7f93ab2957a8a
macintario <macintario@gmail.com>
2019-02-01 15:21:00 -0600

    campos para curp y correcion por modalidad a distancia

--- actdoc.py
+++ actdoc.py
@@ -1,9 +1,29 @@
 import csv
 import os
 
+
+
+#clean all
+#os.remove('salida.csv')
+des_hrs_activ = ''
+ingreso = ''
+cve_act = ''
+actividad = ''
+lugar = ''
+d_lunes = ''
+d_martes = ''
+d_miercoles = ''
+d_jueves = ''
+d_viernes = ''
+d_sabado = ''
+d_domingo = ''
+des_hrs_activ = ''
+
+
 def limpia_escalerita():
     des_hrs_activ = ''
     cve_act = ''
+    ingreso = ''
     actividad = ''
     lugar = ''
     d_lunes = ''
@@ -16,20 +36,6 @@ def limpia_escalerita():
     des_hrs_activ = ''
 
 
-#clean all
-os.remove('salida.csv')
-des_hrs_activ = ''
-cve_act = ''
-actividad = ''
-lugar = ''
-d_lunes = ''
-d_martes = ''
-d_miercoles = ''
-d_jueves = ''
-d_viernes = ''
-d_sabado = ''
-d_domingo = ''
-des_hrs_activ = ''
 
 
 def busca_carrera(pla_id, carr_id):
@@ -52,6 +58,7 @@ with open('RUAS.csv') as csvfile:
         nombre_docente = row[7]
         id_docente = row[6]
         horas_min = row[2]
+        ingreso = row[1]
         horas_max = row[16]
         turno = row[17]
         asi_id = row[28]
@@ -60,8 +67,14 @@ with open('RUAS.csv') as csvfile:
         tipo_asig = row[33]
         semestre = row[34]
         id_pa = row[27]
-        hrs_base = row[42]
-        hrs_interinato = row[41]
+        #presencial
+        if row[43] != '':
+            hrs_base = float(row[43]) + float(row[44])
+
+        ## a distancia
+        if row[41] != '':
+            hrs_interinato = float(row[41])+float(row[38])+float(row[39])+float(row[40])
+
         pza_num_emp = row[25]
         id_grupo = row[36]
         id_mod = row[37]
@@ -80,6 +93,7 @@ with open('RUAS.csv') as csvfile:
             des_hrs_activ = row[62]
         if row[68] != '':
             cve_act = row[68]
+            limpia_escalerita()
         if row[69] != '':
             actividad = row[69]
         if row[70] != '':
@@ -105,17 +119,31 @@ with open('RUAS.csv') as csvfile:
                 linewrite.writerow([pla_id, plantel, nombre_docente, id_docente,horas_min,
                                     horas_max, hrs_nom, turno, asi_id, asignatura, hrs_asig, tipo_asig, semestre, id_pa, busca_carrera(pla_id, id_pa),
                                     hrs_base, hrs_interinato, id_grupo, id_mod, id_salon,
-                                    c_lunes, c_martes, c_miercoles, c_jueves, c_viernes, c_sabado, c_domingo, hrs_total])
+                                    c_lunes, c_martes, c_miercoles, c_jueves, c_viernes, c_sabado, c_domingo, hrs_total, ingreso])
         if des_cic_id != '' and 'CENTRO DE ESTUDIOS' not in plantel:
             with open('salida.csv', mode='a') as salida:
                 linewrite = csv.writer(salida, delimiter=',')
                 linewrite.writerow([pla_id, plantel, nombre_docente, id_docente,horas_min,
                                     horas_max, hrs_nom, turno, cve_act, actividad, hrs_asig, tipo_asig, semestre, id_pa, ' ',
                                     hrs_base, hrs_interinato, id_grupo, 'A', lugar,
-                                    d_lunes, d_martes, d_miercoles, d_jueves, d_viernes, d_sabado, d_domingo, des_hrs_activ])
+                                    d_lunes, d_martes, d_miercoles, d_jueves, d_viernes, d_sabado, d_domingo, des_hrs_activ, ingreso])
+                des_hrs_activ = ''
+                cve_act = ''
+                actividad = ''
+                ingreso = ''
+                lugar = ''
+                d_lunes = ''
+                d_martes = ''
+                d_miercoles = ''
+                d_jueves = ''
+                d_viernes = ''
+                d_sabado = ''
+                d_domingo = ''
+                des_hrs_activ = ''
         if pza_num_emp != '':
             print(pza_num_emp)
             limpia_escalerita()
 print("ok")
+
 
 
